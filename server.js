0bec478883e0963b9df74ac59e9eb3d321d1a7e6
Bohdan Asauliuk <bohdan.asauliuk@gmail.com>
2021-02-02 02:10:41 +0100

    ss

--- server.js
+++ server.js
@@ -6,10 +6,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('/dist'));
+app.use(express.static('/dist/firestarter-demo'));
 
 app.get('/*', function(req,res) {
-    res.sendFile(path.join('/dist/index.html'));
+    res.sendFile(path.join('/dist/firestarter-demo/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
